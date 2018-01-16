2a1ab7d3a9f260608f4e71684215c3aecc7a5901
david.lv <code4funlnyx@gmail.com>
2018-01-16 20:02:02 +0800

    fix 分享二维码重复点击出现多层

--- src/client/util/shareUtil.js
+++ src/client/util/shareUtil.js
@@ -70,6 +70,7 @@ export let showShareQRCode = ({url, sharelog_id}) => {
     console.log(`sharelog_id ${sharelog_id}`);
     let updatedUrl = addParam(url ,{share_mask: 1, sharelog_id});
     let qrcode = `http://www.bstcine.com/qrcode?text=${encodeURIComponent(updatedUrl)}`;
+    hideShareMask()
     let maskNode = document.createElement('div');
     maskNode.className = 'share-mask';
     maskNode.innerHTML = `

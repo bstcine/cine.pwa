0d9cedc6a48c90f331823fbf3737dbdaeaebd759
david <code4funlnyx@gmaul.com>
2018-01-25 18:01:47 +0800

    error code

--- src/client/constant/errorCode.js
+++ src/client/constant/errorCode.js
@@ -1,8 +1,8 @@
 const errorCode = {
+    system_error: '系统繁忙，请尝试刷新或稍后重试',
     username_not_null: '用户名/手机/邮箱不能为空',
     password_not_null: '密码不能为空',
     user_not_exist: '用户名/手机/邮箱或密码有误',
-    system_error: '系统异常，请稍后重试',
     net_error: '网络异常，请稍后重试',
     success: '登录成功',
     login_is_null: '账号为空',

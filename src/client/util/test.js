df938ffb6ad95455f27a842f8ab97c9b3591bd41
david <code4funlnyx@gmail.com>
2018-04-04 18:04:57 +0800

    test

--- src/client/util/test.js
+++ src/client/util/test.js
@@ -1,11 +1,37 @@
-'use strict';
-fn(2)
-var fn = function (a) {
-    console.log(a)
-};
+// 'use strict';
+// fn(2)
+// var fn = function (a) {
+//     console.log(a)
+// };
 //
 // fn(2)
 //
 // var a = function (aaa) {
 //
 // };
+
+// let index = 0;
+
+let query1 = function () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('query1', new Date().getTime())
+            resolve()
+        }, 1000)
+    })
+}
+
+let query2 = function () {
+    return new Promise((resolve, reject) => {
+        console.log('query2', new Date().getTime())
+        resolve()
+    })
+}
+
+let batchQuery = async function () {
+    await query1();
+    await query2();
+}
+
+batchQuery()
+

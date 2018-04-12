44c07ccc3375a57194caaace82060c3c77196f7e
itwangxiang <itwangxiang@foxmail.com>
2018-04-12 15:41:51 +0800

    Tab 添加指示器

--- src/client/component/Tabs/TabItem.js
+++ src/client/component/Tabs/TabItem.js
@@ -7,12 +7,13 @@ export default class TabItem extends Component {
     };
 
     render() {
-        const {id, children, className, index, selected, onTabItemClick} = this.props;
+        const {id, children, className, index, indicator, selected, onTabItemClick} = this.props;
         return (
             <div className={`${className}${selected ? ' active' : ''}`} role="tab-item">
-                <a href="javascript:" onClick={e => onTabItemClick(index,id, e)}>
+                <a href="javascript:" onClick={e => onTabItemClick(index, id, e)}>
                     {children}
                 </a>
+                {indicator && <span className="tab-indicator">{indicator}</span>}
             </div>
         );
     }

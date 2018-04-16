f8c4dcfd340714fcfe9c8076178ca2b4ad7bc19c
joelin109 <joe.lin1009@gmail.com>
2018-04-16 11:31:25 +0800

    Action 规范

--- src/client/action/contentAction.js
+++ src/client/action/contentAction.js
@@ -1,3 +1,24 @@
 /**
  * Created by joe on 4/16/18.
  */
+let cCourseAction = {
+    init: function () {
+        return function (dispatch, getState) {
+
+            let id = getState().id
+            cCourseAction.initWithParam(id)
+        }
+    },
+
+    initWithParam: param => (dispatch, getState) => {
+
+        dispatch({
+            type: "type.Action_UC_initWithParam",
+            payload: param
+        })
+    }
+
+
+};
+
+export default cCourseAction;

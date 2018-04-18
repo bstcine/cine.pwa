bd4c3297e5fb8c87fd3b51c771cb2d5c70c85437
itwangxiang <itwangxiang@foxmail.com>
2018-04-18 15:22:34 +0800

    redux user

--- src/client/action/userAction.js
+++ src/client/action/userAction.js
@@ -12,13 +12,11 @@ const receiveUser = (result) => ({
     type: Action_UI_RECEIVE,
     payload: result,
 })
-export const loadUserInfo = () => dispatch => {
+export const loadUserInfo = () => async dispatch => {
     dispatch(requestUser())
-    return fetchData(Api.APIURL_User_Info, {})
-        .then(([err, result]) => {
-            if (err) return Promise.resolve();
-            return dispatch(receiveUser(result));
-        })
+
+    let [err,result] = await fetchData(Api.APIURL_User_Info)
+    dispatch(receiveUser(result));
 }
 
 const requestCoupon = () => ({
@@ -28,11 +26,10 @@ const receiveCoupon = (result) => ({
     type: Action_UC_RECEIVE,
     payload: result.rows
 })
-export const loadUserCoupon = () => dispatch => {
+export const loadUserCoupon = () => async dispatch => {
     dispatch(requestCoupon())
-    return fetchData(Api.APIURL_User_Coupon, {page: 1, pageSize: 1000000, orderBy: 'create_at', orderValue: 'desc'})
-        .then(([err, result]) => {
-            if (err) return Promise.resolve();
-            return dispatch(receiveCoupon(result));
-        })
+
+    let param = {page: 1, pageSize: 1000000, orderBy: 'create_at', orderValue: 'desc'}
+    let [err,result] = await fetchData(Api.APIURL_User_Coupon, param)
+    dispatch(receiveCoupon(result))
 }

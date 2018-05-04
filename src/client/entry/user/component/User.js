28a1e143deb3aaf79e72728b6129f3c022580872
itwangxiang <itwangxiang@foxmail.com>
2018-05-04 11:08:57 +0800

    code format

--- src/client/entry/user/component/User.js
+++ src/client/entry/user/component/User.js
@@ -3,23 +3,10 @@
  */
 import React from 'react';
 import '../asset/style/index.less';
-import CouponContainer from '@/entry/user/containers/CouponContainer';
-import PointContainer from '@/entry/user/containers/PointContainer';
 import UserHeader from '@/entry/user/component/UserHeader';
 import UserContent from '@/entry/user/component/UserContent';
 
 const User = ({ topicId, isJustUserRoute, user, handleClick }) => {
-    let content = <PointContainer />;
-
-    switch (topicId) {
-        case 'coupon':
-            content = <CouponContainer />;
-            break;
-        case 'integral':
-            content = <PointContainer />;
-            break;
-    }
-
     return (
         <React.Fragment>
             <UserHeader
@@ -28,7 +15,7 @@ const User = ({ topicId, isJustUserRoute, user, handleClick }) => {
                 user={user}
                 handleClick={handleClick}
             />
-            <UserContent isJustUserRoute={isJustUserRoute} content={content} />
+            <UserContent isJustUserRoute={isJustUserRoute} topicId={topicId} />
         </React.Fragment>
     );
 };
--- src/client/entry/user/component/UserContent.js
+++ src/client/entry/user/component/UserContent.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import CouponContainer from '@/entry/user/containers/CouponContainer';
+import PointContainer from '@/entry/user/containers/PointContainer';
 
-const UserContent = ({ content, isJustUserRoute, ...props }) => {
+const UserContent = ({ topicId, isJustUserRoute, ...props }) => {
+    let content = <PointContainer />;
+
+    switch (topicId) {
+        case 'coupon':
+            content = <CouponContainer />;
+            break;
+        case 'integral':
+            content = <PointContainer />;
+            break;
+    }
+
     return (
         <div
             className={isJustUserRoute ? 'user-content just-user' : 'user-content'}>

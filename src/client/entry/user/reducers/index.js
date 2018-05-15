8f468eb406d6acdd3e9d844236995fb42d3f6e62
joelin109 <joe.lin1009@gmail.com>
2018-05-15 14:36:13 +0800

    代码规范

--- src/client/entry/user/reducers/index.js
+++ src/client/entry/user/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
-import { Action_UI, Action_UC, Action_UP } from '@/constant/actionTypeUser';
+import coupons from './couponReducer';
+import points from './pointReducer';
+import { Action_UI } from '@/constant/actionTypeUser';
 
 const user = (
     state = {
@@ -20,49 +22,6 @@ const user = (
     }
 };
 
-const coupons = (
-    state = {
-        rows: [],
-        isOpen: false,
-        network: { loading: false, msg: '', error: '' },
-    },
-    action
-) => {
-    switch (action.type) {
-        case Action_UC.RECEIVE:
-            return { ...state, rows: action.payload };
-        case Action_UC.EXPAND:
-            return {
-                ...state,
-                rows: state.rows.map(item => ({
-                    ...item,
-                    expand: item.id === action.id ? !item.expand : item.expand,
-                })),
-            };
-        case Action_UC.DIALOG_ADD:
-            return { ...state, isOpen: !state.isOpen };
-        case Action_UC.REQUEST_ADD:
-            return { ...state, network: { loading: true, msg: '', error: '' }};
-        case Action_UC.RECEIVE_ADD:
-            return {
-                ...state,
-                network: { loading: false, msg: '添加成功', error: action.err },
-            };
-        case Action_UC.TOAST_HIDE:
-            return { ...state, network: { loading: false, msg: '', error: '' }};
-        default:
-            return state;
-    }
-};
-
-const points = (state = { rows: [], remark: '' }, action) => {
-    switch (action.type) {
-        case Action_UP.RECEIVE:
-            return action.payload;
-        default:
-            return state;
-    }
-};
 
 const rootReducer = combineReducers({
     user,

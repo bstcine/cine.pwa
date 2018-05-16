8362957f56cba3449e3979b63d6740fb2fd41836
itwangxiang <itwangxiang@foxmail.com>
2018-05-16 16:39:31 +0800

    #381: 我的学生 年纪

--- src/client/entry/tgrammar/component/StudentTable.js
+++ src/client/entry/tgrammar/component/StudentTable.js
@@ -3,6 +3,23 @@
  */
 import React from 'react';
 
+const gradeArr = {
+    '0': '学龄前',
+    '1': '一年级',
+    '2': '二年级',
+    '3': '三年级',
+    '4': '四年级',
+    '5': '五年级',
+    '6': '六年级',
+    '7': '七年级',
+    '8': '八年级',
+    '9': '九年级',
+    '10': '十年级',
+    '11': '十一年级',
+    '12': '十二年级',
+    '13': '成人',
+};
+
 const formatTime = time => {
     if (time) {
         return time.substring(2, 16);
@@ -178,8 +195,8 @@ const StudentTable = ({ list, wordsItemClick, quizItemClick, ...props }) => {
             <div key={key} className={'studentPanel'}>
                 <div className={'studentInfo'}>
                     <span>{key + 1}</span>
-                    <span>{item.student_nickname}</span>
-                    <span>{item.student_login.slice(-8)}</span>
+                    <span>{item.student_nickname || item.student_phone}</span>
+                    <span>{gradeArr[item.student_grade] || '暂无'}</span>
                     <span>{item.teacher_nickname}</span>
                 </div>
                 <hr />

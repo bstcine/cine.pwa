57f8dcdb680ba7025d940f35b3d20c12445fa898
joelin109 <joe.lin1009@gmail.com>
2018-05-17 14:14:06 +0800

    代码规范

--- src/client/entry/user/index.js
+++ src/client/entry/user/index.js
@@ -4,26 +4,16 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
-import { grey400, indigo500, indigo700 } from 'material-ui/styles/colors';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import CThemeProvider from '@/component/CThemeProvider';
 import { UserRouter } from './userRouter';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
-const muiTheme = getMuiTheme({
-    palette: {
-        primary1Color: indigo500,
-        primary2Color: indigo700,
-        primary3Color: grey400,
-    },
-});
-
 render(
-    <MuiThemeProvider muiTheme={muiTheme}>
+    <CThemeProvider>
         <Provider store={store}>
             <UserRouter />
         </Provider>
-    </MuiThemeProvider>,
+    </CThemeProvider>,
     document.getElementById('root')
 );
--- src/client/entry/user/userRouter.js
+++ src/client/entry/user/userRouter.js
@@ -1,33 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import CommonUtil from '@/util/common';
+import { CRouter } from '@/component/CRouter';
 import Root from '@/entry/user/containers/Root';
 import CouponContainer from '@/entry/user/containers/CouponContainer';
 import PointContainer from '@/entry/user/containers/PointContainer';
-
-const routes = routes =>
-    routes.map((route, i) => (
-        <Route
-            key={i}
-            path={route.path}
-            render={props => <route.component {...props} />}
-        />
-    ));
-
-const CRouter = ({ route }) => (
-    <Router>
-        <Route
-            path={route.path}
-            render={props => {
-                if (route.willCheckAuth && !CommonUtil.isAuth()) return <div />;
-                return (
-                    <route.component {...props} routes={routes(route.routes)} />
-                );
-            }}
-        />
-    </Router>
-);
 
 const userRoute = {
     path: '/user',

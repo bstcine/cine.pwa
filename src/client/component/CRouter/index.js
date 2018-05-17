d91cd358daca6966bb76352a0b058da6fc6d2487
joelin109 <joe.lin1009@gmail.com>
2018-05-17 14:10:09 +0800

    代码规范

---
+++ src/client/component/CRouter/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CommonUtil from '@/util/common';
+
+const routes = routes =>
+    routes.map((route, i) => (
+        <Route
+            key={i}
+            path={route.path}
+            render={props => <route.component {...props} />}
+        />
+    ));
+
+export const CRouter = ({ route }) => (
+    <Router>
+        <Switch>
+            <Route
+                path={route.path}
+                render={props => {
+                    if (route.willCheckAuth && !CommonUtil.isAuth()) return <div />;
+                    return (
+                        <route.component
+                            {...props}
+                            routes={routes(route.routes)}
+                        />
+                    );
+                }}
+            />
+        </Switch>
+    </Router>
+);

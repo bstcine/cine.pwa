09b2a88553dfe1cd1a5b0ffbc176041fef72036a
david <code4funlnyx@gmail.com>
2018-05-17 17:48:43 +0800

    webpack4.0

---
+++ webpack.common.js
@@ -0,0 +1,109 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+
+const API_Host_URL = 'http://apptest.bstcine.com';
+// const API_Host_URL = 'http://local.bstcine.com:9000';
+const publicPath = '/'; // for cdn
+const pages = ['content', 'cquiz', 'address', 'vocabtest', 'tgrammar', 'user'];
+
+let entry = {};
+let htmlWebpackPlugins = [];
+
+pages.forEach(page => {
+    entry[page] = `./src/client/entry/${page}/index.js`;
+    htmlWebpackPlugins.push(
+        new HtmlWebpackPlugin({
+            filename: `entry/${page}/index.html`,
+            template: `src/client/entry/${page}/index.html`,
+            chunks: ['vendors', 'commons', page],
+        })
+    );
+});
+
+module.exports = {
+    entry,
+    output: {
+        filename: 'entry/[name]/index.[hash:8].js',
+        path: path.resolve(__dirname, 'build'),
+        publicPath,
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            API_Host_URL,
+        }),
+        new CleanWebpackPlugin(['build'], { verbose: devMode }),
+        new MiniCssExtractPlugin({
+            filename: 'entry/[name]/index.[contenthash:8].css',
+        }),
+        ...htmlWebpackPlugins,
+    ],
+    resolve: {
+        modules: [path.resolve(__dirname, 'src/client'), 'node_modules'],
+        alias: {
+            '@': path.resolve(__dirname, 'src/client'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                require('postcss-import'),
+                                require('autoprefixer'),
+                            ],
+                        },
+                    },
+                    'less-loader',
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                require('postcss-import'),
+                                require('autoprefixer'),
+                            ],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                loader: 'url-loader',
+                options: {
+                    limit: 8192,
+                    name: 'asset/image/[name].[hash:8].[ext]',
+                    publicPath,
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|otf|webp|ttf)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: 'asset/font/[name].[hash:8].[ext]',
+                    publicPath,
+                },
+            },
+        ],
+    },
+};
---
+++ webpack.dev.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+const Dev_Host_URL = 'http://apptest.bstcine.com';
+
+let rewrites = [];
+for (let entry in common.entry) {
+    if (common.entry.hasOwnProperty(entry)) {
+        rewrites.push({
+            from: new RegExp(`^/${entry}.*`),
+            to: `/entry/${entry}/index.html`,
+        });
+    }
+}
+rewrites.push({ from: /.*/, to: `/entry/content/index.html` });
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'cheap-module-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'build'),
+        compress: true,
+        hot: true,
+        historyApiFallback: {
+            disableDotRule: true,
+            rewrites,
+        },
+        port: 5000,
+        disableHostCheck: true,
+        host: '0.0.0.0',
+        useLocalIp: true,
+        proxy: {
+            '/api': {
+                target: Dev_Host_URL,
+                secure: false,
+                changeOrigin: true,
+            },
+        },
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+});

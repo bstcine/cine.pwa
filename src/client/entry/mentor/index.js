9ab241c9dd7a74d85602995d719f21d5e640f660
itwangxiang <itwangixang@foxmail.com>
2018-06-27 18:27:53 +0800

    从习题模块拆分导师批改

--- src/client/entry/mentor/index.js
+++ src/client/entry/mentor/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { reduxLogger } from '@/util/loggerUtil';
 
 import rootReducer from './reducer';
 import { MentorRouter } from './mentorRouter';
 
 const preloadedState = window.__PRELOADED_STATE__;
-const store = createStore(rootReducer, preloadedState, applyMiddleware(thunk));
+const store = createStore(rootReducer, preloadedState, applyMiddleware(thunk, reduxLogger));
 
 class Mentor extends Component {
     render() {

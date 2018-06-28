14b5132097b6c874ba041f9b70b3ffa098d4f557
david.lv <code4funlnyx@gmail.com>
2018-06-28 18:05:46 +0800

    update header component

--- src/client/component/GIcon/index.js
+++ src/client/component/GIcon/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import classNames from 'classnames';
-
-const GIcon = ({ name, className }) => {
-    return <i className={classNames('material-icons', className)}>{name}</i>;
+import './style.less';
+/**
+ * mi-xxx --> material-icons
+ * ci-xxx --> cine-icons
+ */
+const GIcon = ({ name, className, url }) => {
+    if (url) {
+        return (
+            <i
+                className={classNames('gicons', className)}
+                style={{
+                    background: `url(${url}) center center / cover no-repeat`,
+                }}
+            />
+        );
+    } else {
+        if (name.indexOf('mi-') === 0) {
+            return (
+                <i className={classNames('gicons material-icons', className)}>
+                    {name.substr(3)}
+                </i>
+            );
+        } else {
+            return (
+                <i
+                    className={classNames('gicons cine-icons', name, className)}
+                />
+            );
+        }
+    }
 };
 
 export default GIcon;
--- src/client/constant/menu.js
+++ src/client/constant/menu.js
@@ -1,14 +1,12 @@
 import * as h5 from './menuItemUrl';
 import { RoleID } from '@/constant';
-// ../image/ico_my_study.png
-// ../image/ico_integral.png
-// ../image/ico_coupon.png
-// ../image/ico_wordtest.png
-// ../image/ico_grammar.png
-// ../image/ico_teacher@2x.png
-// ../image/ico_address@2x.png
-// ../image/ico_edit1.png
-// ../image/ico_quit.png
+/**
+ * ===icon===
+ * mi-xxx --> material-icons
+ * ci-xxx --> cine-icons
+ * 已有样式在 @component/GIcon/style.less
+ * ===icon===
+ */
 const cineMenu = [
     { label: '首页', url: '/' },
     {
@@ -18,8 +16,8 @@ const cineMenu = [
             {
                 label: '我的学习',
                 url: h5.URL_Learn_Index,
-                icon: require('@/entry/user/asset/image/ico_my_study.png'),
-                icon_path: '', // remote url
+                icon: 'ci-my_study',
+                icon_path: '', // remote url or require(local file)
             },
         ],
     },
@@ -30,34 +28,30 @@ const cineMenu = [
             {
                 label: '我的优惠券',
                 url: h5.URL_User_Coupon,
-                icon: require('@/entry/user/asset/image/ico_coupon.png'),
-                icon_path: '',
+                // icon: 'mi-bug_report',
+                icon: 'ci-coupon',
             },
             {
                 label: '我的积分',
                 url: h5.URL_User_Integral,
-                icon: require('@/entry/user/asset/image/ico_integral.png'),
-                icon_path: '',
+                icon: 'ci-integral',
             },
             {
                 label: '我的测试',
                 url: h5.URL_User_Quiz,
-                icon: require('@/entry/user/asset/image/ico_wordtest.png'),
-                icon_path: ''
+                icon: 'ci-grammar',
             },
             {
                 label: '我的订单',
                 url: h5.URL_User_Order,
-                icon: 'payment',
-                icon_path: '',
-                visible: false,
+                icon: 'ci-order',
+                disabled: true,
             },
             {
                 label: '地址管理',
                 url: h5.URL_User_Address,
-                icon: 'add_location',
-                icon_path: '',
-                visible: false,
+                icon: 'ci-address',
+                disabled: true,
             },
         ],
     },
@@ -69,21 +63,19 @@ const cineMenu = [
             {
                 label: '学生作业',
                 url: h5.URL_Mentor_Student_Task,
-                icon: require('@/entry/user/asset/image/ico_integral.png'),
-                icon_path: '',
+                icon_path:
+                    '//www.bstcine.com/asset/image/ico_headpic.d8668494.png',
             },
             {
                 label: '学生测试',
                 url: h5.URL_Mentor_Student_Quiz,
-                icon: require('@/entry/user/asset/image/ico_integral.png'),
-                icon_path: '',
+                icon_path: require('@/entry/user/asset/image/ico_integral.png'),
             },
             {
                 label: '待批改列表',
                 url: h5.URL_Mentor_CorrectList,
-                icon: require('@/entry/user/asset/image/ico_integral.png'),
-                icon_path: '',
-                visible: false,
+                icon_path: require('@/entry/user/asset/image/ico_integral.png'),
+                disabled: true,
             },
         ],
     },

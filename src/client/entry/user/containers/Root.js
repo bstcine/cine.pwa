8245ae7524d12610956545d059545c6c4ff25579
david.lv <code4funlnyx@gmail.com>
2018-06-28 16:03:06 +0800

    remove useless code

--- src/client/entry/user/containers/Root.js
+++ src/client/entry/user/containers/Root.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { actionUserInfo } from '@/action/userAction';
-import { logoutV1 } from '@/service/base';
-import UserMobile from '@/entry/user/component/UserMobile';
 import '../asset/style/index.less';
 import GLayout from '@/component/GLayout';
 import { fetchUserInfo } from '@/action/commonAction';

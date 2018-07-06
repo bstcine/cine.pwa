1d5eb2758a53aee57ffdfc1ef1c3983a2b250e35
itwangxiang <itwangixang@foxmail.com>
2018-07-06 11:12:28 +0800

    mentor route

--- src/client/entry/mentor/container/root.js
+++ src/client/entry/mentor/container/root.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Entry from '@/component/Entry';
 import GLayout from '@/component/GLayout';
+import * as h5 from '@/constant/menuItemUrl';
 
 class Root extends Entry {
+    componentDidMount() {
+        if (location.pathname === h5.URL_Mentor_Index) location.href = h5.URL_Mentor_Student_Task;
+    }
+
     render() {
         const { routes } = this.props;
         return <GLayout>{routes}</GLayout>;

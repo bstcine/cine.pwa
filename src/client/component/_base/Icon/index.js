bef6f7840cdebda63d077387990406da641565cf
joelin109 <joe.lin1009@gmail.com>
2018-07-09 19:25:59 +0800

    demo

--- src/client/component/_base/Icon/index.js
+++ src/client/component/_base/Icon/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import classNames from 'classnames';
+import IconButton from 'material-ui/IconButton';
 
 export const CIcon = ({ name, className }) => {
     return <i className={classNames('material-icons', className)}>{name}</i>;
+};
+
+export const CIconButton = ({ iconName, selected, onClick }) => {
+    const color = selected === true ? 'secondary' : 'primary';
+    const icon = <CIcon name={iconName} color={color} />;
+    return <IconButton onClick={onClick}>{icon}</IconButton>;
 };

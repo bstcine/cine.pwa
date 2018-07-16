144161eef3d1bd0ef868e4a3b703d877235560db
joelin109 <joe.lin1009@gmail.com>
2018-07-16 15:16:46 +0800

    for component demo

--- src/client/component/_base/config.js
+++ src/client/component/_base/config.js
@@ -1,1 +1,33 @@
 export const baseprefix = 'cine';
+export const fontRatio = 5;
+export const cTheme = {
+    palette: {
+        primary: {
+            light: '#ff4400',
+            main: '#ff4400',
+            dark: '#ff4400',
+        },
+        secondary: {
+            light: '#0066ff',
+            main: '#0044ff',
+            dark: '#ffcc00',
+        },
+    },
+    layout: {
+        Panel: {
+            gap: {
+                small: '10px',
+                normal: '20px',
+            },
+        },
+        Card: {
+            gap: {
+                normal: '5px',
+                large: '10px',
+            },
+        },
+    },
+    typography: {
+        fontRatio: 5,
+    },
+};

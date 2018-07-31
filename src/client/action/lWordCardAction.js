91579b33274bab820af887935bc3b261af4d2d77
aaron <sr1991125@126.com>
2018-07-31 19:22:50 +0800

    添加wordCard组件

--- src/client/action/lWordCardAction.js
+++ src/client/action/lWordCardAction.js
@@ -1,7 +1,56 @@
 /**
  * Created by lidangkun on 2018/7/26.
  */
+import Api from '../../APIConfig';
+import { fetchData } from '@/service/base';
+import { ACTION_WC } from '@/constant/actionTypeLearn';
 
 export const lWordCardAction = {
+    _request: () => ({
+        type: ACTION_WC.REQUEST,
+    }),
+
+    _receive: result => ({
+        type: ACTION_WC.RECEIVE,
+        payload: result,
+    }),
+    _autoChangeWordStatus: status => ({
+        type: ACTION_WC.AUTOCHANGEWORDSTATUS,
+        payload: status,
+    }),
+    _reviseChangeWordStatus: status => ({
+        type: ACTION_WC.REVISECHANGEWORDSTATUS,
+        payload: status,
+    }),
+    _changeAutoTime: time => ({
+        type: ACTION_WC.AUTOCHANGETIME,
+        payload: time,
+    }),
     // 卡片式学习方法
+    loadWordList: (param) => async dispatch => {
+
+        let [error, result] = await fetchData(Api.APIURL_User_Learn_Word, param);
+
+        if (!error) {
+            dispatch(lWordCardAction._receive(result));
+        } else {
+            console.log(error);
+        }
+    },
+    // 改变自动播放状态
+    changeAutoChangeWordStatus: () => async (dispatch, getState) => {
+        let reducer = getState().WordCardRedu;
+        let isAutoChangeWord = !reducer.get('isAutoChangeWord');
+        dispatch(lWordCardAction._autoChangeWordStatus(isAutoChangeWord));
+    },
+    // 改变自动播放时间间隔
+    changeAutoChangeTime: (time) => async dispatch => {
+        dispatch(lWordCardAction._changeAutoTime(time));
+    },
+    // 改变随机播放状态
+    changeReviseChangeWordStatus: () => async (dispatch, getState) => {
+        let reducer = getState().WordCardRedu;
+        let isReviseChangeWord = !reducer.get('isReviseChangeWord');
+        dispatch(lWordCardAction._reviseChangeWordStatus(isReviseChangeWord));
+    },
 };
--- src/client/entry/lword/reducer/WordCardReducer.js
+++ src/client/entry/lword/reducer/WordCardReducer.js
@@ -3,14 +3,35 @@
  */
 
 import { fromJS } from 'immutable';
+import { ACTION_WC } from '@/constant/actionTypeLearn';
 
 const initWordCard = fromJS({
-    card: '卡片式学习',
+    result: {
+        name: '',
+        status: 0,
+        rows: null,
+    },
+    isAutoChangeWord: false,
+    isReviseChangeWord: false,
+    autoChangeTime: 3,
 });
 
 const WordCardRedu = (state = initWordCard, action) => {
     // 卡片式学习池子
-    return state;
+    switch (action.type) {
+        case ACTION_WC.REQUEST:
+            return state;
+        case ACTION_WC.RECEIVE:
+            return state.set('result', action.payload);
+        case ACTION_WC.AUTOCHANGEWORDSTATUS:
+            return state.set('isAutoChangeWord', action.payload);
+        case ACTION_WC.REVISECHANGEWORDSTATUS:
+            return state.set('isReviseChangeWord', action.payload);
+        case ACTION_WC.AUTOCHANGETIME:
+            return state.set('autoChangeTime', action.payload);
+        default:
+            return state;
+    }
 };
 
 export default WordCardRedu;

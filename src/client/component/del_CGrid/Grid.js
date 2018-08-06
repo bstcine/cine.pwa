cc4a7e934575b6d408ab97a109839e3c3030f34b
joelin109 <joe.lin1009@gmail.com>
2018-08-06 16:38:39 +0800

    delete

--- src/client/component/del_CGrid/Grid.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classNames from 'classnames';
-import './style.less';
-
-const Grid = ({ className, children }) => (
-    <div className={classNames('cine-grid', className)}>{children}</div>
-);
-
-export default Grid;

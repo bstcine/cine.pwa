9dfab4b41c0b584e9890207c197243282a50ec1a
daweilv <lvdawei1970@gmail.com>
2018-08-10 14:24:07 +0800

    remove useless code

--- src/client/component/del_Mask/index.js
+++
@@ -1,7 +0,0 @@
-// import './index.less';
-// import React from 'react';
-
-// const TransparentMask = () => <div className="cine-mask__tranparent" />;
-// const Mask = () => <div className="cine-mask" />;
-
-// export { Mask, TransparentMask };

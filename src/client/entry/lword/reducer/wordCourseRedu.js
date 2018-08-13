f5bc6a42d2e1abb5e82caefee3ca5e332931e602
joelin109 <joe.lin1009@gmail.com>
2018-08-13 17:08:39 +0800

    word course

--- src/client/entry/lword/reducer/wordCourseRedu.js
+++ src/client/entry/lword/reducer/wordCourseRedu.js
@@ -1,21 +1,23 @@
 import { fromJS } from 'immutable';
 import * as actionType from '@/constant/actionTypeLWord';
 
-const getCourseItems = (courseID, lastID, userQuizRows) => {
-    const indexStart = 1;
-    const indexEnd = 3000;
-    const startedCount = indexStart - 1;
-    const lastVisitID = 1 + 50 * lastID / 50;
+const getCourseItems = (courseID, indexLast, userQuizRows) => {
+    const indexStart = 3001;
+    const wordCount = 3000;
+    const lastVisitID = 1 + parseInt((indexLast - indexStart) / 50, 10);
 
     let items = [];
-    for (let i = 0; i < indexEnd / 50; i++) {
-        const id = startedCount + i * 50 + 1;
+    for (let i = 0; i < wordCount / 50; i++) {
+        const id = i + 1;
+        const _from = indexStart + i * 50;
+        const value = _from + '-' + (_from + 49);
         const wQuizScore = -1;
         const wKnownCount = 0;
         const isLastVisit = id === lastVisitID;
 
         const item = {
             id,
+            value,
             wQuizScore,
             wKnownCount,
             isLastVisit,
@@ -28,9 +30,9 @@ const getCourseItems = (courseID, lastID, userQuizRows) => {
 
 const initWordCourse = fromJS({
     userID: null,
-    courseID: '1-3000',
+    courseID: '3001-3000',
     lastVisitID: 502,
-    rows: getCourseItems('1-3000', 502),
+    rows: getCourseItems('3001-3000', 3502),
 });
 
 const WordCourseRedu = (state = initWordCourse, action) => {

65d18e12d69ba42aaa5e99fac6684d2c6b4279f3
daweilv <lvdawei1970@gmail.com>
2018-08-22 18:08:14 +0800

    update 学习内页

--- src/client/entry/learn/component/CourseContent.js
+++ src/client/entry/learn/component/CourseContent.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { CPanel, CIcon } from '@/component/_base';
+import { withRouter } from 'react-router';
 
-const CourseContent = ({}) => {
-    return (
-        <CPanel
-            title="《新概念英语2》第2单元精讲（25-48课）"
-            className="course__content">
-            
-        </CPanel>
-    );
-};
+class CourseContent extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log(nextProps, 'nextProps');
 
-export default CourseContent;
+        return true;
+    }
+
+    render() {
+        console.log('render CourseContent');
+
+        return (
+            <CPanel
+                title="《新概念英语2》第2单元精讲（25-48课）"
+                className="course__content">
+                1231
+            </CPanel>
+        );
+    }
+}
+
+export default withRouter(CourseContent);

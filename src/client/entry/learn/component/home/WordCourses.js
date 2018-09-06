6f4f02150e055379e1d8fdb1e95bb6ac7efd4881
joelin109 <joe.lin1009@gmail.com>
2018-09-06 16:41:07 +0800

    word course

--- src/client/entry/learn/component/home/WordCourses.js
+++ src/client/entry/learn/component/home/WordCourses.js
@@ -3,47 +3,35 @@ import { CPanel, CCard, CCardContainer } from '@/component/_base';
 import '@/entry/learn/asset/style/homeWordCourse.less';
 
 const WordCourses = ({ courses }) => {
-    const getWordCourseH = (startIndex, range) => {
-        return (
-            <div className="course">
-                <div className="top">善恩核心10000词</div>
-                <div className="middle">{`${startIndex}-${range}`}</div>
-                <div className="bottom">
-                    <div className="linkButton">立即背单词</div>
-                </div>
-            </div>
-        );
-    };
-
     const getWordCourse = course => {
-        // alert(JSON.stringify(course));
-        let url = `/lword/course?start_index=1&range=10000`;
-        return (
-            <CCard hover="lighten">
-                <div className="course">
-                    <a href={url} target="_blank">
-                        <div className="img-c">
-                            <div
-                                className="img"
-                                style={{
-                                    background: `url(${
-                                        course.cover
-                                    })center center / cover no-repeat `,
-                                }}
-                            />
-                        </div>
-                    </a>
+        const imgbg = `url(${course.cover}) center center / cover no-repeat`;
+        if (course.status === 1) {
+            return (
+                <a href={course.href} target="_blank">
+                    <div className="img-c">
+                        <div className="img" style={{ background: `${imgbg}` }} />
+                    </div>
+                </a>
+            );
+        } else {
+            return (
+                <div className="img-c">
+                    <div className="img" style={{ background: `${imgbg}` }} />
                 </div>
-            </CCard>
-        );
+            );
+        }
     };
 
     let cards = courses.map((course, i) => {
-        return getWordCourse(course);
+        return (
+            <CCard key={i}>
+                <div className="course">{getWordCourse(course)}</div>
+            </CCard>
+        );
     });
     return (
-        <CPanel title="背单词">
-            <CCardContainer gap="large" layout="234">
+        <CPanel title="核心词汇">
+            <CCardContainer gap="large" layout="234" line="1">
                 {cards}
             </CCardContainer>
         </CPanel>

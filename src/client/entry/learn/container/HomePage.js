26406caabcd354c2d4b18c4e75e0806074f075ec
daweilv <lvdawei1970@gmail.com>
2018-09-12 11:21:48 +0800

    remove -webkit-touch-callout

--- src/client/entry/learn/container/HomePage.js
+++ src/client/entry/learn/container/HomePage.js
@@ -6,7 +6,6 @@ import '@/entry/learn/asset/style/index.less';
 import { withRouter } from 'react-router-dom';
 import { GLayoutContainer } from '@/g/container';
 import WordCourses from './../component/home/WordCourses';
-import { CPanel } from '@/component/_base';
 import * as wordCourse from '@/service/data/response_word_course.json';
 
 const mapStateToProps = state => {

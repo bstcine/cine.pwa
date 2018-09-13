1f777275009509c687a128192c65a57b9450e65a
daweilv <lvdawei1970@gmail.com>
2018-09-13 16:55:09 +0800

    修复app内由于Header组件未挂载导致的User对象缺少的问题

--- src/client/entry/learn/container/HomePage.js
+++ src/client/entry/learn/container/HomePage.js
@@ -7,6 +7,7 @@ import { withRouter } from 'react-router-dom';
 import { GLayoutContainer } from '@/g/container';
 import WordCourses from './../component/home/WordCourses';
 import * as wordCourse from '@/service/data/response_word_course.json';
+import gAction from '@/g/action';
 
 const mapStateToProps = state => {
     const { userRedu } = state;
@@ -17,6 +18,10 @@ const mapStateToProps = state => {
 };
 
 class HomePage extends Component {
+    componentDidMount() {
+        this.props.dispatch(gAction.preFetchUserInfo());
+    }
+
     render() {
         const { user, courses } = this.props;
         return (

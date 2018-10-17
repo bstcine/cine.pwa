cd0d1ba1ea1af1058870506a4179067c74c5306c
daweilv <lvdawei1970@gmail.com>
2018-10-17 10:23:03 +0800

    色值修改

--- src/client/entry/lword/index.js
+++ src/client/entry/lword/index.js
@@ -7,24 +7,6 @@ import { Provider } from 'react-redux';
 import rootReducer from './reducer';
 import { WordRouter } from './wordRouter';
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker
-            .register('/sw-learn.js', { scope: '/learn' })
-            .then(function(registration) {
-                // 注册成功
-                console.log(
-                    'ServiceWorker registration successful with scope: ',
-                    registration.scope
-                );
-            })
-            .catch(function(err) {
-                // 注册失败:(
-                console.log('ServiceWorker registration failed: ', err);
-            });
-    });
-}
-
 const preloadedState = window.__PRELOADED_STATE__;
 const store = createStore(rootReducer, preloadedState, applyMiddleware(thunk));
 

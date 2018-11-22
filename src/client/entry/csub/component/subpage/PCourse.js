c7b94279b0b1f3e38e03e9dacd0af9362b405550
joelin109 <joe.lin1009@gmail.com>
2018-11-22 17:05:25 +0800

    sp

--- src/client/entry/csub/component/subpage/PCourse.js
+++ src/client/entry/csub/component/subpage/PCourse.js
@@ -2,14 +2,6 @@ import React from 'react';
 import { CPanel, CCardContainer } from '@/component/_base';
 import { CourseList } from '@/component/CardItem';
 
-const CourseExList = ({ isCourse, courses, actions }) => {
-    const layout = isCourse ? '234' : '112';
-    let exList = <CourseList list={courses} hover="lighten" />;
-
-    return <CCardContainer layout={layout}>{exList}</CCardContainer>;
-};
-// export default CourseExList;
-
 export default class PCourse extends React.PureComponent {
     render() {
         const { isCourse, list } = this.props;

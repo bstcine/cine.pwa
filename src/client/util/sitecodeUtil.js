8fe50787ae717ed7c3529175ae7c970580b308d1
joelin109 <joe.lin1009@gmail.com>
2018-12-13 17:09:23 +0800

    Kt&H5

--- src/client/util/sitecodeUtil.js
+++ src/client/util/sitecodeUtil.js
@@ -33,6 +33,14 @@ let siteCodeUtil = {
     inAPP: () => {
         return siteCodeUtil.inIOSAPP() || siteCodeUtil.inAndroidAPP();
     },
+
+    /**
+     * 确保入口文件的 constructor 函数中 set 过 sitecode
+     *  storeUtil.setSiteCode(sitecode);
+     */
+    inAndroidH5: () => {
+        return storeUtil.getSiteCode() === SITECODE.ANDROID_H5;
+    },
 };
 
 export default siteCodeUtil;

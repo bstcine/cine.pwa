588b296f7be10d3c3228f1c65788a04e28185778
daweilv <lvdawei1970@gmail.com>
2019-01-07 17:32:17 +0800

    fix signin style

--- src/client/component/Auth/Social.js
+++ src/client/component/Auth/Social.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './style.less';
 import { CButton, CIcon, CMessage } from '@/component/_base';
 import { fetchData } from '@/service/base';
 import {

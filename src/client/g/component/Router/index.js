ae0cb745a6afc7a1861a3c770bb60c1793670f3a
daweilv <lvdawei1970@gmail.com>
2019-01-08 10:41:23 +0800

    crouter

--- src/client/g/component/Router/index.js
+++ src/client/g/component/Router/index.js
@@ -1,45 +1,22 @@
 import React from 'react';
-import { BrowserRouter, HashRouter, Route } from 'react-router-dom';
+import CRouter from '@/component/CRouter';
 import storeUtil from '@/util/_base/storeUtil';
 
-const RRDRouter = ({ children }) => {
-    return MODE === 'static' ? (
-        <HashRouter>{children}</HashRouter>
-    ) : (
-        <BrowserRouter>{children}</BrowserRouter>
-    );
-};
+const GRouter = ({ routes }) => (
+    <CRouter
+        routes={routes}
+        render={(props, item) => {
+            if (item.checkAuth && !storeUtil.getToken()) {
+                location.replace(
+                    '/auth/signin?redirect=' + encodeURIComponent(location.href)
+                );
+                return <div key={item.path} />;
+            } else {
+                const Comp = item.component;
+                return <Comp {...props} />;
+            }
+        }}
+    />
+);
 
-const Routes = ({ routes }) => {
-    return routes.map(item => (
-        <Route
-            exact={!!item.exact}
-            key={item.path}
-            path={item.path}
-            render={props => {
-                if (item.checkAuth && !storeUtil.getToken()) {
-                    location.replace(
-                        '/auth/signin?redirect=' +
-                            encodeURIComponent(location.href)
-                    );
-                    return <div key={item.path} />;
-                } else {
-                    const Comp = item.component;
-                    return <Comp {...props} />;
-                }
-            }}
-        />
-    ));
-};
-
-const Router = ({ routes }) => {
-    return (
-        <RRDRouter>
-            <React.Fragment>
-                <Routes routes={routes} />
-            </React.Fragment>
-        </RRDRouter>
-    );
-};
-
-export default Router;
+export default GRouter;

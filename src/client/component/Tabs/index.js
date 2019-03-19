f7dceed8b0b07c9010436503f90fc41853438a71
daweilv <lvdawei1970@gmail.com>
2019-03-19 10:09:39 +0800

    fix: tab.less 引用修改

--- src/client/component/Tabs/index.js
+++ src/client/component/Tabs/index.js
@@ -3,5 +3,6 @@ import TabItems from './TabItems';
 import TabItem from './TabItem';
 import TabPanels from './TabPanels';
 import TabPanel from './TabPanel';
+import './tabs.less';
 
 export { Tabs, TabItems, TabItem, TabPanels, TabPanel };

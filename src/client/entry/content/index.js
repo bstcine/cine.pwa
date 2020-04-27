3017995594f82206edc042cbf7e26f9eaffb99ea
daweilv <lvdawei1970@gmail.com>
2020-04-27 17:25:38 +0800

    feat: 放开app下载提示

--- src/client/entry/content/index.js
+++ src/client/entry/content/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './asset/style/index.less';
-// import appBanner from '@/util/appBanner';
+import appBanner from '@/util/appBanner';
 import Entry from '@/component/Entry';
 import routes from './routes';
 import { GRouter } from '@/g/component';
@@ -10,20 +10,20 @@ class Content extends Entry {
     constructor(props) {
         super(props);
         console.log('Content Main constructor');
-        // this.handleLoad = this.handleLoad.bind(this);
+        this.handleLoad = this.handleLoad.bind(this);
     }
 
-    // componentDidMount() {
-    //     window.addEventListener('load', this.handleLoad);
-    // }
-    //
-    // componentWillUnmount() {
-    //     window.removeEventListener('load', this.handleLoad);
-    // }
-    //
-    // handleLoad() {
-    //     appBanner.init();
-    // }
+    componentDidMount() {
+        window.addEventListener('load', this.handleLoad);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('load', this.handleLoad);
+    }
+
+    handleLoad() {
+        appBanner.init();
+    }
 
     render() {
         return <GRouter routes={routes} />;
